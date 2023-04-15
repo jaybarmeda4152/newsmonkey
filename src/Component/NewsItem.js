c247dc60520fb111c66a73241e1d8c0cae793c70
jaybarmeda4152 <121751261+jaybarmeda4152@users.noreply.github.com>
2023-04-15 17:59:39 +0530

    add news card

--- src/Component/NewsItem.js
+++ src/Component/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title, description, imageUrl, newsUrl} = this.props;
     return (
       <div>
-        This is news item.
+        <div className="card" style={{width: "18rem"}}>
+            <img src={imageUrl} className="card-img-top" alt="NewsApp-Image"/>
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+                <a href={newsUrl} target='_blank' className="btn btn-primary">Read More...</a>
+            </div>
+        </div>
       </div>
     )
   }
