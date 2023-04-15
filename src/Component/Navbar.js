566c0032110e1113f203670d2f916d2150da426f
jaybarmeda4152 <121751261+jaybarmeda4152@users.noreply.github.com>
2023-04-15 19:38:02 +0530

    add next and prev button

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -3,13 +3,13 @@ import React from 'react'
 export default function Navbar() {
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/">NewsApp</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="" id="navbarNavAltMarkup">
                         <div className="navbar-nav">
                             <a className="nav-link active" aria-current="page" href="/">Home</a>
                             <a className="nav-link" href="/">About</a>
--- src/Component/News.js
+++ src/Component/News.js
@@ -5,14 +5,41 @@ export class News extends Component {
     constructor() {
         super();
         this.state = {
-            articles: []
+            articles: [],
+            isLoading:false,
+            page:1
         }
     }
     async componentDidMount() {
-        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=3e9982abaf574078b2aed66269f6b9b8"
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=3e9982abaf574078b2aed66269f6b9b8&page=1"
         let data = await fetch(url);
         let objNewsData = await data.json()
-        this.setState({ articles: objNewsData.articles });
+        this.setState({ 
+            articles: objNewsData.articles,
+            totalResults : objNewsData.totalResults
+         });
+    }
+    handlePrevClick = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=3e9982abaf574078b2aed66269f6b9b8&page=${this.state.page - 1}`
+        let data = await fetch(url);
+        let objNewsData = await data.json()
+        this.setState({ 
+            articles: objNewsData.articles,
+            page: this.state.page + 1
+         });
+    }
+    handleNextClick = async () => {
+        if( this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+
+        }else{
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=3e9982abaf574078b2aed66269f6b9b8&page=${ this.state.page + 1 }`
+            let data = await fetch(url);
+            let objNewsData = await data.json()
+            this.setState({ 
+                articles: objNewsData.articles,
+                page: this.state.page + 1
+            });
+        }
     }
     render() {
         return (
@@ -26,6 +53,10 @@ export class News extends Component {
                             </div>
                         )
                     })}
+                </div>
+                <div className="d-flex justify-content-between my-3">
+                    <button disabled={this.state.page<=1} className="btn btn-dark" type='button' onClick={this.handlePrevClick} >&larr; Previous</button>
+                    <button  className="btn btn-dark" type='button' onClick={this.handleNextClick} >Next &rarr;</button>
                 </div>
             </div>
         )
